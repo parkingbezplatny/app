d93214a0d9584f9620905cb78b681c21e20d783e
Adrian Rudnicki <adi2602@wp.pl>
2023-10-16 23:14:53 +0200

    add session provider

--- lib/providers/Providers.tsx
+++ lib/providers/Providers.tsx
@@ -2,13 +2,18 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </ChakraProvider>
+    <SessionProvider>
+      <ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </ChakraProvider>
+    </SessionProvider>
   );
 }
