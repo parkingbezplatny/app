788f443646fcf7a1c0480e4f1c3f08056ef5d673
Zayooo <mrcold135@gmail.com>
2023-10-16 20:21:06 +0200

    Created initial views #2 #3 #4 (#5)
    * initial run update
    
    * added website icon
    
    * updated global css file
    
    * removed unnecessary files
    
    * created welcome page view
    
    * created sign in page view
    
    * created sign up page view

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,61 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import React from "react";
+import { Box, Heading, Text, Button, Flex, Image } from "@chakra-ui/react";
 
-export default function Home() {
+function WelcomePage() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="90vh"
+      textAlign="center"
+      mx={4}
+    >
+      <Image
+        src="welcome-page-parking.jpg"
+        alt="Dwie osoby na tle samochodu i znaku parkingu"
+        mb={4}
+        w={{ base: 300, md: 400 }}
+        h={{ base: 200, md: 300 }}
+      />
+      <Heading fontSize={{ base: "2xl", md: "3xl" }} mb={4}>
+        Witaj w naszym serwisie z darmowymi parkingami!
+      </Heading>
+      <Text fontSize={{ base: "sm", md: "xl" }}>
+        Cieszymy się, że jesteś tutaj. Rozpocznij wyszukiwanie idealnego miejsca
+        parkingowego już teraz!
+      </Text>
+      <Flex mt={4}>
+        <Link href="/pages/sign-in">
+          <Button
+            size={{ base: "sm", md: "md" }}
+            bg="orange.400"
+            _hover={{
+              bg: "#DD6B20",
+            }}
+            textColor="white"
+            mr={4}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            Zaloguj się
+          </Button>
+        </Link>
+        <Link href="/pages/sign-up">
+          <Button
+            size={{ base: "sm", md: "md" }}
+            bg="orange.500"
+            _hover={{
+              bg: "#C05621",
+            }}
+            textColor="white"
+          >
+            Zarejestruj się
+          </Button>
+        </Link>
+      </Flex>
+    </Box>
   );
 }
+
+export default WelcomePage;
