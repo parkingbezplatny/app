964327a1a5ea28951a676d013e2e6e14c92a5f63
Zayooo00 <mrcold135@gmail.com>
2023-10-19 12:54:30 +0200

    split sidepanel into seperate component

--- app/(pages)/(user)/dashboard/page.tsx
+++ app/(pages)/(user)/dashboard/page.tsx
@@ -1,24 +1,17 @@
 "use client";
 
+import React, { useState } from "react";
 import {
   Box,
-  Heading,
-  Input,
-  List,
-  Text,
-  Divider,
-  Button,
-  Center,
-  HStack,
-  Image,
   useBreakpointValue,
-  IconButton,
   Slide,
+  IconButton,
+  Image,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-import Navbar from "@/app/components/navbar";
+import Navbar from "components/navbar";
+import SidePanel from "components/sidepanel";
 
 function Dashboard() {
   const [tab, setTab] = useState("favorites");
@@ -58,89 +51,7 @@ function Dashboard() {
             direction="left"
             in={true}
           >
-            <Box
-              minW="250px"
-              height="calc(100dvh - 83px)"
-              p={5}
-              borderRight="1px solid #A0AEC0"
-              bg="white"
-            >
-              <Center justifyContent="space-evenly" mb={2}>
-                <Button
-                  bg={"white"}
-                  _hover={{
-                    bg: "#f4f4f5",
-                  }}
-                  onClick={() => setTab("favorites")}
-                >
-                  Ulubione
-                </Button>
-                <Button
-                  bg={"white"}
-                  _hover={{
-                    bg: "#f4f4f5",
-                  }}
-                  onClick={() => setTab("search")}
-                >
-                  Wyszukaj
-                </Button>
-              </Center>
-              <HStack gap={0}>
-                <Divider
-                  mb={4}
-                  borderBottom={
-                    tab === "favorites" ? "2px solid black" : "2px solid grey"
-                  }
-                />
-                <Divider
-                  mb={4}
-                  borderBottom={
-                    tab === "search" ? " 2px solid black" : "2px solid grey"
-                  }
-                />
-              </HStack>
-
-              {tab === "favorites" && (
-                <>
-                  <Heading as="h3" size="md" mb={4}>
-                    Ulubione
-                  </Heading>
-                  <List mb={5}>
-                    {parkings.map((parking, index) => (
-                      <>
-                        <Divider mb={2} borderColor="gray.200" />
-                        <Text fontWeight={"semibold"} key={index}>
-                          {parking.name}
-                        </Text>
-                        <Text key={index}>{parking.city}</Text>
-                        <Text
-                          fontSize={"sm"}
-                          fontStyle={"italic"}
-                          mb={3}
-                          key={index}
-                        >
-                          {parking.coordinates.lat}, {parking.coordinates.lng}
-                        </Text>
-                      </>
-                    ))}
-                  </List>
-                  <Divider mb={2} borderColor="gray.200" />
-                </>
-              )}
-
-              {tab === "search" && (
-                <>
-                  <Heading as="h3" size="md" mb={4}>
-                    Wyszukaj
-                  </Heading>
-                  <Input
-                    placeholder="Wyszukaj parking..."
-                    focusBorderColor="orange.500"
-                    mb={5}
-                  />
-                </>
-              )}
-            </Box>
+            <SidePanel tab={tab} setTab={setTab} parkings={parkings} />
           </Slide>
         ) : null}
         {!isLargerThanLG && (
