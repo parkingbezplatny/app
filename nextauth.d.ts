1f2a140449ef1a5ff8a1ecc7d2e18eb29dcbd143
Adrian Rudnicki <adi2602@wp.pl>
2023-10-19 16:02:00 +0200

    create custom object for NextAuth

---
+++ nextauth.d.ts
@@ -0,0 +1,29 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface User extends DefaultUser {
+    id: string;
+    email: string;
+    username: string;
+    isAdmin: boolean;
+  }
+
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      email: string;
+      username: string;
+      isAdmin: boolean;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id: string;
+    email: string;
+    username: string;
+    isAdmin: boolean;
+  }
+}
