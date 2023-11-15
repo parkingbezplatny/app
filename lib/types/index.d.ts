c38d20c027963c193c2c1ce58308c68e5a5c6fe5
Adrian Rudnicki <adi2602@wp.pl>
2023-11-15 14:07:53 +0100

    new types

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,60 +1,49 @@
 import * as z from "zod";
 import { SignInValidation } from "../validations/forms/signIn.validation";
 import { SignUpValidation } from "../validations/forms/signUp.validation";
+import {
+  Address,
+  FavoriteParkingAndUser,
+  Geometry,
+  Parking,
+  Properties,
+  RatingParkingAndUser,
+  User,
+} from "@prisma/client";
+import { UpdateParkingValidation } from "../validations/forms/updateParking.validation";
+import { CreateParkingValidation } from "../validations/forms/createParking.validation";
+import { UpdateUserUsernameValidation } from "../validations/forms/updateUserUsername.validation";
+import { UpdateUserPasswordValidation } from "../validations/forms/updateUserPassword.validation";
 
-export type TUserDatabase = {
-  email: string;
-  id: number;
-  username: string;
-  isGoogle: boolean;
-  isAdmin: boolean;
-  favoriteParkings: {
-    parking: {
-      id: number;
-      lat: number;
-      lng: number;
-      place: string;
-    };
-  }[];
-};
+export type TUser = {
+  favoriteParkings: FavoriteParkingAndUser[] | null;
+  parkingRatings: RatingParkingAndUser[] | null;
+} & Omit<User, "password">;
 
-export type TUsersDatabase = {
-  email: string;
-  id: number;
-  username: string;
-  isGoogle: boolean;
-  isAdmin: boolean;
-  favoriteParkings: {
-    parking: {
-      id: number;
-      lat: number;
-      lng: number;
-      place: string;
-    };
-  }[];
-}[];
+export type TCreateUser = z.infer<typeof SignUpValidation>;
+export type TUpdateUserUsername = z.infer<typeof UpdateUserUsernameValidation>;
+export type TUpdateUserPassword = z.infer<typeof UpdateUserPasswordValidation>;
 
-export type TParkingDatabase = {
-  userRatings: {
-    rating: number;
-  }[];
-} & {
-  id: number;
-  lat: number;
-  lng: number;
-  place: string;
-};
+export type TParking = {
+  favoriteUsers: FavoriteParkingAndUser[] | null;
+  userRatings: RatingParkingAndUser[] | null;
+  properties:
+    | ({
+        address: Address | null;
+      } & Properties)
+    | null;
+  geometry: Geometry | null;
+} & Parking;
 
-export type TParkingsDatabase = ({
-  userRatings: {
-    rating: number;
-  }[];
-} & {
-  id: number;
-  lat: number;
-  lng: number;
-  place: string;
-})[];
+export type TParkingMap = {
+  properties: {
+    address: Pick<Address, "label"> | null;
+  } | null;
+  geometry: Pick<Geometry, "coordinates" | "type"> | null;
+} & Parking;
+
+export type TCreateParking = z.infer<typeof CreateParkingValidation>;
+export type TUpdateParking = z.infer<typeof UpdateParkingValidation>;
 
 export type TSignInForm = z.infer<typeof SignInValidation>;
 export type TSignUpForm = z.infer<typeof SignUpValidation>;
--- nextauth.d.ts
+++ nextauth.d.ts
@@ -1,30 +1,23 @@
+import { FavoriteParkingAndUser, RatingParkingAndUser } from "@prisma/client";
 import { DefaultSession, DefaultUser } from "next-auth";
 import { DefaultJWT } from "next-auth/jwt";
+import { TUser } from "./lib/types";
+
+interface CustomUser extends TUser {}
 
 declare module "next-auth" {
   interface User extends DefaultUser {
-    id: string;
-    email: string;
-    username: string;
-    isAdmin: boolean;
+    id?: string;
+    user: CustomUser;
   }
 
   interface Session extends DefaultSession {
-    user: {
-      id: string;
-      email: string;
-      username: string;
-      isAdmin: boolean;
-    };
+    user: CustomUser;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT extends DefaultJWT {
-    id: string;
-    email: string;
-    username: string;
-    isAdmin: boolean;
+    user: CustomUser;
   }
 }
-
