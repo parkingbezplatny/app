b5f2655af090867b6ffbd208f707224189e9573b
Zayooo <mrcold135@gmail.com>
2023-11-16 13:13:52 +0100

    Modify navbar to use menu instead of button #103 (#104)
    * displaying actual username in profile component
    
    * replaced logout button with avatar menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,9 +12,15 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Icon,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import { FiMenu } from "react-icons/fi";
+import { FiMenu, FiLogOut } from "react-icons/fi";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
 
@@ -33,6 +39,8 @@ const Navbar = () => {
       wrap="wrap"
       padding="1.25rem"
       borderBottom="1px solid #A0AEC0"
+      position="relative"
+      zIndex="dropdown"
     >
       <Link href="/dashboard">
         <Flex align="center">
@@ -54,23 +62,37 @@ const Navbar = () => {
         display={{ base: "none", md: "block" }}
         flexBasis={{ base: "100%", md: "auto" }}
       >
-        <Flex align="center">
-          <Text w={"120px"} fontSize={"sm"} fontWeight={"bold"} mx={2}>
-            {session?.user?.username}
-          </Text>
-          <Button
-            bg="orange.400"
-            _hover={{
-              bg: "#DD6B20",
-            }}
-            textColor="white"
-            w="100px"
-            size="md"
-            onClick={logout}
-          >
-            Wyloguj się
-          </Button>
-        </Flex>
+        <Box zIndex={999}>
+          <Menu z-index="3">
+            <MenuButton
+              as={Button}
+              rounded={"full"}
+              variant={"link"}
+              cursor={"pointer"}
+            >
+              <Avatar
+                bg="white"
+                color="black"
+                shadow="lg"
+                borderWidth={1}
+                borderColor={"blackAlpha.400"}
+                boxSize={10}
+                name={session?.user?.username}
+              />
+            </MenuButton>
+            <MenuList>
+              <Link href="/profile">
+                <MenuItem>Profil</MenuItem>
+              </Link>
+              <MenuItem onClick={logout}>
+                <Box display="flex">
+                  Wyloguj się
+                  <Icon ml={2} mt={"4.5px"} as={FiLogOut} />
+                </Box>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
       </Box>
 
       <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
@@ -79,16 +101,29 @@ const Navbar = () => {
           <VStack align="center" justify="center" mt={5} mx={4}>
             <DrawerCloseButton mt={2} />
             <Text mt={8} fontSize={"sm"}>
-              Aktualnie zalogowany jako:
+              Aktualnie zalogowano jako:
             </Text>
             <Text mb={4} fontSize={"sm"} fontWeight={"bold"}>
               {session?.user?.username}
             </Text>
             <Button
+              as={Link}
+              href="/profile"
               size="sm"
               bg="orange.400"
               _hover={{
-                bg: "#DD6B20",
+                bg: "orange.500",
+              }}
+              textColor="white"
+              w={"full"}
+            >
+              Profil
+            </Button>
+            <Button
+              size="sm"
+              bg="orange.500"
+              _hover={{
+                bg: "orange.600",
               }}
               textColor="white"
               w={"full"}
