87a3fb063a3663035adb6d8944ec05f48bcc1a01
Zayooo <mrcold135@gmail.com>
2023-11-16 12:10:49 +0100

    Create profile page #99 (#102)
    * created profile page
    
    * created password change modal
    
    * created username change modal
    
    * created modal props type
    
    * implemented modal props type

---
+++ components/username-modal.tsx
@@ -0,0 +1,59 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+
+import { TModalProps } from "@/lib/types";
+
+export default function UsernameModal({ isOpen, onClose }: TModalProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent mx={2}>
+        <ModalHeader>Zmień nazwę użytkownika</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormControl>
+            <FormLabel>Nowa nazwa użytkownika</FormLabel>
+            <Input
+              focusBorderColor="orange.400"
+              placeholder="Nazwa użytkownika"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel mt={4}>Powtórz nową nazwę użytkownika</FormLabel>
+            <Input
+              focusBorderColor="orange.400"
+              placeholder="Powtórz nazwę użytkownika"
+            />
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            bg="orange.500"
+            _hover={{
+              bg: "orange.600",
+            }}
+            textColor="white"
+            mr={3}
+            onClick={onClose}
+          >
+            Save
+          </Button>
+          <Button variant="ghost" onClick={onClose}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
