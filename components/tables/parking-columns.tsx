18e2ba2816aed0435c1e1301c0373c118df194ad
Adrian Rudnicki <adi2602@wp.pl>
2023-11-19 22:10:21 +0100

    something

--- components/tables/parking-columns.tsx
+++ components/tables/parking-columns.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TParkingMap } from "@/lib/types";
+import { TParking, TParkingMap } from "@/lib/types";
 import { Button, Flex } from "@chakra-ui/react";
 import { ColumnDef } from "@tanstack/react-table";
 import { FiEdit } from "react-icons/fi";
@@ -8,7 +8,7 @@ import { AiOutlineDelete } from "react-icons/ai";
 import UpdateParkingModal from "../parking/update-parking-modal";
 import DeleteParkingModal from "../parking/delete-parking-modal";
 
-export const parkingColumns: ColumnDef<Omit<TParkingMap, "geometry">>[] = [
+export const parkingColumns: ColumnDef<TParking>[] = [
   {
     accessorKey: "id",
     header: "ID",
@@ -27,29 +27,10 @@ export const parkingColumns: ColumnDef<Omit<TParkingMap, "geometry">>[] = [
       const parking = row.original;
 
       return (
-        <Flex direction="row" gap="1rem">
+        <Flex direction="row" gap="1rem" justify="flex-end">
           <UpdateParkingModal parkingId={parking.id} />
           <DeleteParkingModal parkingId={parking.id} />
         </Flex>
-        // <DropdownMenu>
-        //   <DropdownMenuTrigger asChild>
-        //     <Button variant="ghost" className="h-8 w-8 p-0">
-        //       <span className="sr-only">Open menu</span>
-        //       <MoreHorizontal className="h-4 w-4" />
-        //     </Button>
-        //   </DropdownMenuTrigger>
-        //   <DropdownMenuContent align="end">
-        //     <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        //     <DropdownMenuItem
-        //       onClick={() => navigator.clipboard.writeText(payment.id)}
-        //     >
-        //       Copy payment ID
-        //     </DropdownMenuItem>
-        //     <DropdownMenuSeparator />
-        //     <DropdownMenuItem>View customer</DropdownMenuItem>
-        //     <DropdownMenuItem>View payment details</DropdownMenuItem>
-        //   </DropdownMenuContent>
-        // </DropdownMenu>
       );
     },
   },
