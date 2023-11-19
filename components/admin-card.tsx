81a3d092f5ad676a3dd053387a8bdb36acb9dd21
Adrian Rudnicki <adi2602@wp.pl>
2023-11-19 22:09:14 +0100

    for admin navigation

---
+++ components/admin-card.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+} from "@chakra-ui/react";
+import React from "react";
+
+interface AdminCardProps {
+  header: string;
+  body: React.ReactNode;
+  footer?: React.ReactNode;
+}
+
+function AdminCard({ header, body, footer }: AdminCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size="md">{header}</Heading>
+      </CardHeader>
+      <CardBody>{body}</CardBody>
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+}
+
+export default AdminCard;
--- components/admin-sidepanel.tsx
+++ components/admin-sidepanel.tsx
@@ -1,38 +1,55 @@
+"use client";
+
 import React from "react";
-import {
-  Box,
-  Center,
-  Button,
-  HStack,
-  Divider,
-  Heading,
-  List,
-  Text,
-  Input,
-  VStack,
-  Flex,
-} from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function AdminSidePanel() {
+  const pathname = usePathname();
+
+  console.log(pathname);
   return (
     <Box
-      pt={5}
+      p={2}
       minW="230px"
       height="calc(100dvh - 83px)"
       borderRight="1px solid #A0AEC0"
       bg="white"
     >
-      <Flex direction={"column"} gap={"2rem"}>
-        <Link href="user">
-          <Box w={"100%"} p={5}>
-            Użytkownicy
-          </Box>
+      <Flex direction={"column"} gap={"1rem"} boxSizing={"border-box"}>
+        <Heading size="sm">Admin panel</Heading>
+        <Link href="/admin">
+          <Flex
+            p={2}
+            w={"min-content"}
+            borderBottom={pathname === "/admin" ? "2px solid orange" : ""}
+            mb={pathname === "/admin" ? "" : "2px"}
+          >
+            Dashboard
+          </Flex>
         </Link>
-        <Link href="parking">
-          <Box w={"100%"} p={5}>
+        <Link href="/admin/user">
+          <Flex
+            p={2}
+            w={"min-content"}
+            borderBottom={pathname.includes("user") ? "2px solid orange" : ""}
+            mb={pathname.includes("user") ? "" : "2px"}
+          >
+            Użytkownicy
+          </Flex>
+        </Link>
+        <Link href="/admin/parking">
+          <Flex
+            p={2}
+            w={"min-content"}
+            borderBottom={
+              pathname.includes("parking") ? "2px solid orange" : ""
+            }
+            mb={pathname.includes("parking") ? "" : "2px"}
+          >
             Parkingi
-          </Box>
+          </Flex>
         </Link>
       </Flex>
     </Box>
