5de3339fb55518104a2fb582441e3010bb3261ba
B34TER <sew.szymanski@gmail.com>
2023-11-20 22:53:08 +0100

    Add ServerFunctionResponse classes

---
+++ lib/helpers/server-function-response.tsx
@@ -0,0 +1,25 @@
+export class ServerFunctionResponse<T> {
+  success: boolean;
+  message: string;
+  data: T;
+
+  constructor(success: boolean, message: string, data: T) {
+    this.success = success;
+    this.message = message;
+    this.data = data;
+  }
+}
+
+export class SuccessServerFunctionResponse<
+  T,
+> extends ServerFunctionResponse<T> {
+  constructor(message: string, data: T) {
+    super(true, message, data);
+  }
+}
+
+export class ErrorServerFunctionResponse extends ServerFunctionResponse<null> {
+  constructor(message: string) {
+    super(false, message, null);
+  }
+}
