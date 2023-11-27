6767c330b1d57918564d58afd7a4fb8b9b49fe73
B34TER <sew.szymanski@gmail.com>
2023-11-27 22:51:43 +0100

    Add useUpdateUsername hook

---
+++ lib/hooks/userHooks.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import agent from "../api/agent";
+import { TUpdateUserUsername } from "../types";
+
+export type TUseUpdateUsernameProps = {
+    email: string;
+  } & TUpdateUserUsername
+
+function updateUsername({ email, username }: TUseUpdateUsernameProps) {
+    return agent.Users.updateUsernameByEmail(email, {
+      username: username,
+    });
+  }
+
+export const useUpdateUsername = (onSuccess: () => void) => {
+    return useMutation({
+        mutationFn: updateUsername,
+        onSuccess: onSuccess,
+      });
+}
+
