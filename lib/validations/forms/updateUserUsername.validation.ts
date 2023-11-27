ababc112b31dea3cbc25e38b832e0d60288d0083
B34TER <sew.szymanski@gmail.com>
2023-11-27 22:50:34 +0100

    Update UpdateUserUsernameValidation

--- lib/validations/forms/updateUserUsername.validation.ts
+++ lib/validations/forms/updateUserUsername.validation.ts
@@ -4,7 +4,4 @@ export const UpdateUserUsernameValidation = z.object({
   username: z
     .string({ required_error: "Nazwa użytkownika jest wymagana" })
     .min(1, { message: "Nazwa użytkownika nie może być pusta" }),
-  password: z
-    .string({ required_error: "Hasło jest wymagane" })
-    .min(8, { message: "Hasło musi zawierać minumum 8 znaków" }),
 });
