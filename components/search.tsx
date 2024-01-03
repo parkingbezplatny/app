c2e356fbec3c5ccaae85a14bd01cdd3b0acc65f3
Zayooo00 <mrcold135@gmail.com>
2024-01-03 13:17:50 +0100

    added error handling and displaying for search component

--- components/search.tsx
+++ components/search.tsx
@@ -2,7 +2,7 @@
 
 import useDebounce from "@/lib/hooks/useDebounce";
 import { TParking } from "@/lib/types";
-import { Box, Flex, Heading, Input, Spinner } from "@chakra-ui/react";
+import { Box, Flex, Heading, Input, Spinner, Text } from "@chakra-ui/react";
 import axios from "axios";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 
@@ -15,9 +15,11 @@ function Search({
   const [searchParkings, setSearchParkings] = useState<TParking[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const debouncedInputValue = useDebounce(inputValue, 500);
+  const [error, setError] = useState<string | null>(null);
 
   const getParkings = async () => {
     setLoading(true);
+    setError(null);
     if (!debouncedInputValue) {
       setSearchParkings([]);
       setLoading(false);
@@ -28,7 +30,9 @@ function Search({
       .then((res) => {
         setSearchParkings(res.data.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setError(err.response.data.message);
+      });
     setLoading(false);
   };
 
@@ -58,16 +62,26 @@ function Search({
           <Flex justify="center" align="center">
             <Spinner />
           </Flex>
+        ) : error ? (
+          <Text mt={-1} textAlign="center">{error}</Text>
         ) : (
-          <Box maxH="50dvh" overflowY="auto">
+          <Box
+            rounded="md"
+            border={searchParkings.length > 0 ? "1px solid #d8dce4" : "none"}
+            px={2}
+            maxH="50vh"
+            overflowY="auto"
+          >
             {searchParkings.map((parking) => (
               <Flex
                 key={parking.id}
-                py="0.5rem"
+                py="0.75rem"
                 borderBottom="1px"
-                borderColor="gray.400"
+                borderColor="#d8dce4"
+                _last={{ borderBottom: "none" }}
               >
                 <Box
+                  w="full"
                   p="0.2rem"
                   cursor="pointer"
                   _hover={{ backgroundColor: "#dddddd" }}
