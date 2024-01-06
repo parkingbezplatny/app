54a6eb1d74c1e9e29e055f201a2a0c99692b2f5e
Adrian Rudnicki <92043491+adiko2602@users.noreply.github.com>
2024-01-06 15:39:27 +0100

    protect with middleware (#129)

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    if (
+      req.nextUrl.pathname === "/admin" &&
+      !req.nextauth.token?.user.isAdmin
+    ) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: (params) => {
+        let { token } = params;
+        return !!token;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    "/admin/:path*",
+    "/profile/:path*",
+    "/dashboard/:path*",
+    "/api/favorite/:path*",
+    "/api/parkings/:path*",
+    "/api/users/:path*",
+  ],
+};
