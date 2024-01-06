ed3b6dae602cdb950a9deb9b88e6857d0ae6248e
Adrian Rudnicki <92043491+adiko2602@users.noreply.github.com>
2024-01-06 15:05:35 +0100

    #126 parking panel for admin (#127)
    * idk
    
    * api route for password change
    
    * handle error more specific
    
    * page for change
    
    * scrollbar also for x axis
    
    * keep data false for fast refresh
    
    * put to patch
    
    * consume parkings

--- components/parking/update-parking-modal.tsx
+++ components/parking/update-parking-modal.tsx
@@ -11,6 +11,7 @@ import {
 import { useRef } from "react";
 import { FiEdit } from "react-icons/fi";
 import UpdateParkingForm from "./update-parking-form";
+import { useGetParking } from "@/lib/hooks/parkingHooks";
 
 type Props = {
   parkingId: number;
@@ -18,6 +19,10 @@ type Props = {
 
 function UpdateParkingModal({ parkingId }: Props) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { data: parkingResponse } = useGetParking(
+    parkingId ? parkingId.toString() : ""
+  );
 
   const initialRef = useRef(null);
 
@@ -33,7 +38,15 @@ function UpdateParkingModal({ parkingId }: Props) {
           <ModalHeader>Edytuj parking</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <UpdateParkingForm parkingId={parkingId} onClose={onClose}/>
+            {parkingResponse ? (
+              <UpdateParkingForm
+                parkingId={parkingId}
+                parkingResponse={parkingResponse}
+                onClose={onClose}
+              />
+            ) : (
+              <div>Loading...</div>
+            )}
           </ModalBody>
         </ModalContent>
       </Modal>
