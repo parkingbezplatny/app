08bccd07f2dbd534af0fd24d23ab3e312f92ef93
Adrian Rudnicki <adi2602@wp.pl>
2024-01-08 01:39:05 +0100

    queryClient out of Providers

---
+++ lib/query/queryClient.ts
@@ -0,0 +1,56 @@
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
+import { ApiResponse } from "../helpers/server-function-response";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (err: unknown) => {
+      if (err instanceof AxiosError) {
+        const data = err.response?.data as ApiResponse<unknown>;
+        //   toast({
+        //     title: getErrorMessage(data.message),
+        //     status: "error",
+        //     isClosable: true,
+        //   });
+      }
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (err: unknown) => {
+      let message = "";
+      if (
+        err !== null &&
+        typeof err === "object" &&
+        "message" in err &&
+        "data" in err
+      ) {
+        message = err.message as string;
+      } else {
+        if (err instanceof AxiosError) {
+          const data = err.response?.data as ApiResponse<unknown>;
+          message = data.message;
+        }
+      }
+      // toast({
+      //   title: getErrorMessage(message),
+      //   status: "error",
+      //   isClosable: true,
+      // });
+    },
+    onSuccess: (data: unknown) => {
+      let message = "";
+      if (typeof data === "object" && data !== null && "message" in data) {
+        message = data.message as string;
+      } else {
+        const dataResponse = data as AxiosResponse;
+        const dataFromResponse = dataResponse.data as ApiResponse<unknown>;
+        message = dataFromResponse.message;
+      }
+      // toast({
+      //   title: getErrorMessage(message),
+      //   status: "success",
+      //   isClosable: true,
+      // });
+    },
+  }),
+});
