3d784be3f9e6643c10342dbbacab9635b31080da
Adrian Rudnicki <92043491+adiko2602@users.noreply.github.com>
2024-01-08 01:40:58 +0100

    #131 map as context to prevent again loading (#134)
    * map context
    
    * retrun all favoriteParkingQuery
    
    * split favorite from parking
    
    * use map context
    
    * fix import
    
    * use map context
    
    * use favorite
    
    * maby map loader fix
    
    * queryClient out of Providers

--- lib/providers/Providers.tsx
+++ lib/providers/Providers.tsx
@@ -12,62 +12,10 @@ import { getErrorMessage } from "../helpers/getErrorMessage";
 import { ApiResponse } from "../helpers/server-function-response";
 import { AxiosError, AxiosResponse } from "axios";
 import { MapContextProvider } from "../context/mapContext";
+import { queryClient } from "../query/queryClient";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const toast = useToast();
-
-  const queryClient = new QueryClient({
-    queryCache: new QueryCache({
-      onError: (err: unknown) => {
-        if (err instanceof AxiosError) {
-          const data = err.response?.data as ApiResponse<unknown>;
-          toast({
-            title: getErrorMessage(data.message),
-            status: "error",
-            isClosable: true,
-          });
-        }
-      },
-    }),
-    mutationCache: new MutationCache({
-      onError: (err: unknown) => {
-        let message = "";
-        if (
-          err !== null &&
-          typeof err === "object" &&
-          "message" in err &&
-          "data" in err
-        ) {
-          message = err.message as string;
-        } else {
-          if (err instanceof AxiosError) {
-            const data = err.response?.data as ApiResponse<unknown>;
-            message = data.message;
-          }
-        }
-        toast({
-          title: getErrorMessage(message),
-          status: "error",
-          isClosable: true,
-        });
-      },
-      onSuccess: (data: unknown) => {
-        let message = "";
-        if (typeof data === "object" && data !== null && "message" in data) {
-          message = data.message as string;
-        } else {
-          const dataResponse = data as AxiosResponse;
-          const dataFromResponse = dataResponse.data as ApiResponse<unknown>;
-          message = dataFromResponse.message;
-        }
-        toast({
-          title: getErrorMessage(message),
-          status: "success",
-          isClosable: true,
-        });
-      },
-    }),
-  });
+  // const toast = useToast();
 
   return (
     <SessionProvider>
